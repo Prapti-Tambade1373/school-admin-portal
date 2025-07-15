5ed9c21f0aae4238f8a6ad3b2784ee745ca82cf5
Prapti Thambade <praptitambade313@gmail.com>
2025-07-15 07:09:35 +0530

    Add files via upload

---
+++ EmployeeDashboard.jsx
@@ -0,0 +1,12 @@
+import TeacherTable from './TeacherTable';
+
+function EmployeeDashboard() {
+  return (
+    <div>
+      <h3 style={{color:"white"}} className="text-center mb-4">Employee Dashboard</h3>
+      <TeacherTable />
+    </div>
+  );
+}
+
+export default EmployeeDashboard;
